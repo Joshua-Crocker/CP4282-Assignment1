54335feb4c03654599ce3df3a2758d683254a6e5
Joshua Crocker <47287266+Jasta1234@users.noreply.github.com>
2024-05-16 15:48:16 -0230

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import movies from './movies.json';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <NavBar></NavBar>
+    <Movie props={movies}></Movie>
+   </>
   );
 }
+
+function NavBar() {
+  const [loginState, setloginState] = useState("Login", "Logout")
+  return (
+    <>
+      <h1>Assignment 1</h1>
+      <Button onClick={ (evt) => {
+        loginState === "Login" ? setloginState("Logout") : setloginState("Login");
+      }} variant="dark">{loginState}</Button>{' '}
+    </>
+  )
+}
+
+function Movie ( {props} ) {
+  return (
+    <>
+      {props.movies.map( (name) => {
+      return <MovieList props={name}></MovieList>
+    })}
+    </>
+  );
+}
+
+function MovieList( {props} ) {
+  return (
+    <>
+      <p>{props.name}</p>
+      <p>{props.year}</p>
+      <p>{props.rating}</p>
+      <br></br>
+    </>
+  );
+}
+
 
 export default App;
